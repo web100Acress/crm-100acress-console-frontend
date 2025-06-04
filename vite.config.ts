19651963c814d3657a8fbf22582f9e03585aba63
ashutosh04k <kumarasutosh2014@gmail.com>
2025-06-04 17:18:04 +0530

    feat: Add vite-tsconfig-paths for improved TypeScript path resolution

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
   server:{
     port:5000,
   },
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),tailwindcss(),tsconfigPaths()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
